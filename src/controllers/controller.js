f451a5183d78c71ae712b3a32c20a6ac467bd267
psyche <cl598@njit.edu>
2021-05-09 17:12:04 -0400

    retrieve a single object

--- src/controllers/controller.js
+++ src/controllers/controller.js
@@ -51,7 +51,17 @@ exports.findAll = (req, res) => {
 
 // Find a single Tutorial with an id
 exports.findOne = (req, res) => {
+    const id = req.params.id;
 
+    Tutorial.findByPk(id)
+        .then(data => {
+            res.send(data);
+        })
+        .catch(err => {
+            res.status(500).send({
+                message: "Error retrieving Tutorial with id=" + id
+            });
+        });
 };
 
 // Update a Tutorial by the id in the request
